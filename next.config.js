8394d4054b76f6df8ba1178c7b2b20baf614bb13
locnt1 <locnt1@dtp-education.com>
2023-02-20 13:46:05 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     BASE_URL: "https://nextjs-blog-app-keuc-mpq9c28s5-locng0114.vercel.app/api",
+
+  },
+  rewrites () {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://nextjs-blog-app-keuc-mpq9c28s5-locng0114.vercel.app/api/:path*',
+      },
+    ]
   }
 }
 
