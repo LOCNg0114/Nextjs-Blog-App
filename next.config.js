188a75672d4063217788ee959a9d93a365d5f0e7
locnt1 <locnt1@dtp-education.com>
2023-02-20 13:49:24 +0700

    update URL domain

--- next.config.js
+++ next.config.js
@@ -2,17 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    BASE_URL: "https://nextjs-blog-app-keuc-mpq9c28s5-locng0114.vercel.app/api",
+    BASE_URL: "https://nextjs-blog-app-keuc.vercel.app/api",
 
   },
-  rewrites () {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://nextjs-blog-app-keuc-mpq9c28s5-locng0114.vercel.app/api/:path*',
-      },
-    ]
-  }
 }
 
 module.exports = nextConfig
